f82ec9369fee79c314c4861516446f2ec69288cc
JeongHun0612 <56680804+JeongHun0612@users.noreply.github.com>
2025-03-21 18:20:05 +0900

    Create Lobby System

---
+++ Log-Kill/Assets/Scripts/UI/Window/OnlineModeWindow.cs
@@ -0,0 +1,44 @@
+using LogKill.LobbySystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LogKill.UI
+{
+    public class OnlineModeWindow : WindowBase
+    {
+        [SerializeField] private TMP_InputField _lobbyCodeField;
+        [SerializeField] private Button _joinButton;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _joinButton.interactable = false;
+
+            _lobbyCodeField.onValueChanged.AddListener(value =>
+            {
+                _joinButton.interactable = !string.IsNullOrEmpty(value);
+            });
+        }
+
+        public void OnClickCreateLobby()
+        {
+            var createLobbyWindow = UIManager.Instance.ShowWindow<CreateLobbyWindow>();
+            createLobbyWindow.Initialize();
+        }
+
+        public void OnClickLobbyList()
+        {
+            // TODO ShowWIndow LobbyListWindow
+            Debug.Log("TODO: ShowWIndow LobbyListWindow");
+        }
+
+        public async void OnClickLobbyJoin()
+        {
+            string lobbyCode = _lobbyCodeField.text;
+
+            await LobbyManager.Instance.JoinLobbyAsync(lobbyCode);
+        }
+    }
+}
