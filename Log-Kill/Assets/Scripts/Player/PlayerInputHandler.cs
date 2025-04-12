7afe15691e887132398f72d86d6bab8b50600467
JeongHun <56680804+JeongHun0612@users.noreply.github.com>
2025-04-12 15:12:25 +0900

    Fix PlayerInputHandler

--- Log-Kill/Assets/Scripts/Player/PlayerInputHandler.cs
+++ Log-Kill/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,28 +1,20 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace LogKill.Character
 {
-    public class PlayerInputHandler : MonoBehaviour
+    public class PlayerInputHandler : NetworkBehaviour
     {
-        private bool _isOwner;
-
         public Vector2 MoveDirection { get; private set; } = Vector2.zero;
 
-        public void Initialize(bool isOwner)
+        public void Initialize()
         {
-            _isOwner = isOwner;
-            if (_isOwner)
-            {
-                GetComponent<PlayerInput>().SwitchCurrentControlScheme("Keyboard&Mouse", Keyboard.current, Mouse.current);
-            }
+            GetComponent<PlayerInput>().SwitchCurrentControlScheme("Keyboard&Mouse", Keyboard.current, Mouse.current);
         }
 
         private void OnMove(InputValue value)
         {
-            if (!_isOwner)
-                return;
-
             MoveDirection = value.Get<Vector2>();
         }
 
